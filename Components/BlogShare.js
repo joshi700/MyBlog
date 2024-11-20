a2e9643d69c90ec1e54ec21cc4af7026ab7e4cc6
GJ <joshi700@gmail.com>
2024-11-20 18:01:57 +0530

    Update BlogShare.js

--- Components/BlogShare.js
+++ Components/BlogShare.js
@@ -12,8 +12,8 @@ function BlogShare({ data }) {
             target="_blank"
             href={`https://x.com/intent/tweet?text=${
               data.Title
-            } by @soumyajit4419
-            &url=blogs.soumya-jit.tech/blogs/${String(
+            } by @gauravjoshi700
+            &url=my-blog-wheat-iota.vercel.app/blogs/${String(
               data.Title.split(" ").join("-").toLowerCase()
             )}
             &hashtags=${data.Tags.split(" ")}`}
